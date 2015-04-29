83fc98470f75f2093d0a66a346cbb06b0fdbd4ba
Kiryl Kalasouski <kirylk>
2015-04-29 17:26:59 +0300

    updated

--- courseproject/app/passport/init.js
+++ courseproject/app/passport/init.js
@@ -4,7 +4,6 @@ var User = require('../models/user');
 
 module.exports = function(passport){
 
-    // Passport needs to be able to serialize and deserialize users to support persistent login sessions
     passport.serializeUser(function(user, done) {
         console.log('serializing user: ');console.log(user);
         done(null, user._id);
@@ -20,5 +19,4 @@ module.exports = function(passport){
     // Setting up Passport Strategies for Login and SignUp/Registration
     login(passport);
     signup(passport);
-
 }
